ae3ebab1344e6d1353c85f73d2a37de820886ab1
Ahmadi423 <125839045+Ahmadi423@users.noreply.github.com>
2025-10-16 20:31:21 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MonteRojoHome from './MonteRojoHome'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MonteRojoHome />
+  </React.StrictMode>
+)
